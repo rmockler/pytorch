6bee5f3fb6fd5a45a3ce0c4133249237a30b496a
R Mockler <rmockler@gmail.com>
2020-11-03 01:01:48 +0000

    first commit

---
+++ create_this_repo.rm.sh
@@ -0,0 +1,7 @@
+echo "# pytorch" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rmockler/pytorch.git
+git push -u origin main
